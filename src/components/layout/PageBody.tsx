6f38be8537ce21dcc8e4d5efad3f2ea1ae807ef1
Hung Doan <hungdoansy@gmail.com>
2024-08-21 23:13:10 +0700

    feat: impl PageHeader

--- src/components/layout/PageBody.tsx
+++ src/components/layout/PageBody.tsx
@@ -1,5 +1,5 @@
 const PageBody = ({ children }: React.PropsWithChildren) => {
-    return <main className="min-h-full flex-1 mt-8 mb-8">{children}</main>
+    return <main className="min-h-full flex-1 mt-8 mb-8 pt-48">{children}</main>
 }
 
 export default PageBody
--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -1,20 +1,66 @@
-import ThemeToggle from "@/components/ThemeToggle"
-import PageContainer from "@/components/layout/PageContainer"
+"use client"
 
+import { Tooltip } from "@maximeheckel/design-system"
+import { motion } from "framer-motion"
+import Link from "next/link"
+import { useState } from "react"
+
+import Logo from "@/components/Logo"
+import CommandCenterButton from "@/components/buttons/CommandCenterButton"
+import LightDarkSwitcher from "@/components/buttons/LightDarkSwitcher"
+import useScrollCounter from "@/hooks/useScrollCounter"
+import { cn } from "@/lib/utils"
+
+const headerVariants = {
+    open: {
+        height: 120,
+        transition: { ease: "easeInOut", duration: 0.3 },
+    },
+    collapsed: {
+        height: 60,
+        transition: { ease: "easeInOut", duration: 0.3, delayChildren: 0.5 },
+    },
+}
+
+const offsetHeight = 120
+const showProgressBarOnMobile = true
+const title = ""
 const PageHeader: React.FC = () => {
-    return (
-        <div className="w-full h-16 flex justify-center bg-muted-foreground">
-            <PageContainer className="flex items-center justify-between">
-                <div>
-                    <a className="rootLink" href="/">
-                        /home/hung
-                    </a>
-                    <span className="animate-pulse">❙</span>
-                </div>
+    const reached = useScrollCounter(offsetHeight / 2)
+    const [isSearchOpen, setIsSearchOpen] = useState(false)
 
-                <ThemeToggle />
-            </PageContainer>
-        </div>
+    return (
+        <Tooltip.Provider>
+            <header
+                className={cn(
+                    "w-full flex justify-center bg-muted-foreground fixed top-0 left-0 z-10",
+                    reached ? "border-b" : ""
+                )}
+            >
+                <motion.div
+                    initial="open"
+                    animate={reached ? "collapsed" : "open"}
+                    variants={headerVariants}
+                    className={cn("w-full flex items-center justify-between max-w-[840px]")}
+                >
+                    <div className="w-full flex items-center justify-between">
+                        <div className="flex items-center">
+                            <Link aria-label="Home" aria-describedby="home" href="/">
+                                <Logo alt="Logo" size={40} />
+                            </Link>
+                        </div>
+
+                        <div className="flex items-center gap-3">
+                            <CommandCenterButton isSearchShown={isSearchOpen} onClick={() => setIsSearchOpen(true)} />
+                            <LightDarkSwitcher />
+                        </div>
+                    </div>
+                    {/* {showProgressBarOnMobile ? (
+                        <HeaderProgressBar css={{ "--progress": `${readingProgress * 100}%` }} />
+                    ) : null} */}
+                </motion.div>
+            </header>
+        </Tooltip.Provider>
     )
 }
 
